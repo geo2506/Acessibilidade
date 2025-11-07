db19302b70a0ce249881e2d77c1de49aa2923ec4
geo2506 <geovana.santos25@escola.pr.gov.br>
2025-11-07 11:01:02 -0300

    carousel.js

---
+++ js/carousel.js
@@ -0,0 +1,10 @@
+var btns = document.querySelectorAll('.listaDeArtigo-slide-item');
+
+//percorrer todos os botoes controladores 
+btns.addEventlistener('click' , function)}
+
+
+//remover class 'ativo' dos outros botoes 
+btns.forEach(function(btnRemoveclass){
+    btnRemoveclass.classList.remove(listadeartigos-slider-item-ativo)
+}
